246cff818fb450885032d44c39f770d43266d806
Pablo González <nordiws@gmail.com>
2023-08-14 21:30:01 -0300

    login, register and reset refactor

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { UserStore } from '@/stores/user'
 import { useRouter, useRoute } from 'vue-router'
+import { NButton, NInput, NCard, NSpace, NIcon, NTag } from 'naive-ui'
 
 const userStore = UserStore()
 const router = useRouter()
@@ -39,10 +40,84 @@ const LogIn = () => {
       error.value = err.message
     })
 }
+
+watch(error, (newVal) => {
+  if (newVal) {
+    setTimeout(() => {
+      error.value = null
+    }, 5000)
+  }
+})
 </script>
 
 <template>
-  <div class="container d-flex ai-center hv-100 px-50">
+  <n-space
+    justify="center"
+    align="center"
+    class="hv-100"
+    style="background-color: rgb(233, 241, 233)"
+  >
+    <n-card title="Acessar o Muda">
+      <n-tag v-if="error" closable class="mb-10" type="error" @close="error = null">
+        {{ error }}
+      </n-tag>
+      <n-space vertical>
+        <n-input
+          id="email"
+          type="text"
+          class="form-control"
+          name="email"
+          placeholder="E-mail"
+          required
+          autofocus
+          v-model="email"
+        />
+
+        <n-input
+          id="password"
+          type="password"
+          class="form-control"
+          name="password"
+          placeholder="Senha"
+          required
+          v-model="password"
+        />
+      </n-space>
+      <template #footer>
+        <div>
+          Novo por aqui?
+          <router-link to="/register">Registre-se</router-link>
+        </div>
+
+        <div>
+          Esqueceu a senha?
+          <router-link to="/reset-password">Recuperar</router-link>
+        </div>
+      </template>
+      <template #action>
+        <n-space>
+          <n-button type="primary" @click="LogIn">Entrar</n-button>
+          <n-button type="info" @click="GoogleLogin">
+            <template #icon>
+              <n-icon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 488 512"
+                >
+                  <path
+                    d="M488 261.8C488 403.3 391.1 504 248 504C110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6c98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </n-icon>
+            </template>
+          </n-button>
+        </n-space>
+      </template>
+    </n-card>
+  </n-space>
+  <!--  <div class="container d-flex ai-center hv-100 px-50">
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
@@ -110,7 +185,7 @@ const LogIn = () => {
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style scoped lang="scss">
