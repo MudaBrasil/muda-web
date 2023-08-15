d4edd763d0f77d0291a9ec7f85dc8e7e12ee9856
Rennan Moreira <rennanmore@gmail.com>
2023-08-14 22:41:45 -0300

    remove comments in code

--- src/views/users/ResetPassword.vue
+++ src/views/users/ResetPassword.vue
@@ -70,55 +70,6 @@ watch(error, (newVal) => {
       </template>
     </n-card>
   </n-space>
-
-  <!--   <div class="container d-flex ai-center hv-100 px-50">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Recuperação de senha</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <br />
-            <form action="#" @submit.prevent="ResetPassword">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Enviar email</button>
-                </div>
-              </div>
-              <br />
-              <br />
-              <br />
-              <div class="card-header">
-                Ja possui uma conta?
-                <router-link to="/login">Entrar</router-link>
-              </div>
-
-              <div class="card-header">
-                Novo por aqui?
-                <router-link to="/register">Registre-se</router-link>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <style scoped lang="scss">
