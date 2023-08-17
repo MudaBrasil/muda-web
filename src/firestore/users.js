3c375cd0e82b5f52bab4b175fb408457f2ac0f14
Pablo González <nordiws@gmail.com>
2023-08-16 22:59:35 -0300

    cleaning console.logs

--- src/firestore/users.js
+++ src/firestore/users.js
@@ -11,7 +11,6 @@ const saveUser = async (user) => {
 
 const getUsers = async () => {
   try {
-    console.log('getUsers')
     const querySnapshot = await getDocs(collection(db, 'users'))
     const users = querySnapshot.docs.map((doc) => doc.data())
     return users
@@ -24,7 +23,7 @@ const getUserById = async (userId) => {
   try {
     const userDoc = await doc(db, 'users', userId).get()
     if (userDoc.exists()) {
-      return { id: userDoc.id, ...userDoc.data() }
+      return userDoc.data()
     } else {
       console.log('User not found')
       return null
@@ -38,7 +37,6 @@ const updateUser = async (userId, updatedUser) => {
   try {
     const userRef = doc(db, 'users', userId)
     await updateDoc(userRef, updatedUser)
-    console.log('User updated successfully')
   } catch (error) {
     console.log(error)
   }
@@ -48,7 +46,6 @@ const deleteUser = async (userId) => {
   try {
     const userRef = doc(db, 'users', userId)
     await deleteDoc(userRef)
-    console.log('User deleted successfully')
   } catch (error) {
     console.log(error)
   }
