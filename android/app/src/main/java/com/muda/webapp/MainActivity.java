a1a45da1734ab0f8913ca66e5a52355cb597ef86
Rennan Moreira <rennanmore@gmail.com>
2023-08-17 14:39:12 -0300

    install cordova geolocation

---
+++ android/app/src/main/java/com/muda/webapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.muda.webapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <script setup>
 import imgTreeBackground from '@/assets/tree_background.png'
+import { ref } from 'vue'
+import { Geolocation } from '@capacitor/geolocation'
+
+const loc = ref({ lat: null, long: null })
+
+const getCurrentPosition = async () => {
+  const pos = await Geolocation.getCurrentPosition()
+  loc.value = {
+    lat: pos.coords.latitude,
+    long: pos.coords.longitude
+  }
+}
 </script>
 
 <template>
@@ -13,6 +25,15 @@ import imgTreeBackground from '@/assets/tree_background.png'
         <br />
         <img src="@/assets/logo.png" alt="Logo do Muda" />
         <br />
+      </div>
+
+      <div>
+        <h1>Geolocation</h1>
+        <p>Your location is:</p>
+        <p>Latitude: {{ loc.lat }}</p>
+        <p>Longitude: {{ loc.long }}</p>
+
+        <button @click="getCurrentPosition">Get Current Location</button>
       </div>
 
       <div class="context-field">
