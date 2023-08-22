ade194c827b67f4fa6f2e3db3fd81ee4768487a0
Rennan Moreira <rennanmore@gmail.com>
2023-08-21 23:44:48 -0300

    adding sentry

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
-
+import * as Sentry from '@sentry/vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
@@ -29,5 +29,21 @@ pinia.use(piniaPluginPersistedState)
 
 app.use(pinia)
 app.use(router)
-
+Sentry.init({
+  app,
+  dsn: 'https://e56e2192fb1ed5c1156526bf674cc6f4@o4505745942708224.ingest.sentry.io/4505745945460736',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', 'https://muda.education/'],
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+    }),
+    new Sentry.Replay()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,82 +1,96 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      workbox: {
-        cleanupOutdatedCaches: false
-      },
-      injectRegister: 'auto',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        theme_color: '#114c7c',
-        background_color: '#114c7c', //  114C7C 1B5477
-        display: 'standalone',
-        scope: '/',
-        lang: 'pt',
-        start_url: '/',
-        name: 'Muda',
-        short_name: 'Muda',
-        description: 'Seu melhor jeito para aprender feliz!',
-        icons: [
-          {
-            src: '/logo-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: '/logo-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/logo-bg.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ],
-        dir: 'auto',
-        categories: [
-          'education',
-          'fitness',
-          'food',
-          'games',
-          'health',
-          'music',
-          'productivity',
-          'social',
-          'utilities'
-        ]
+export default ({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const p = { env: { ...process.env, ...loadEnv(mode, process.cwd()) } }
+
+  return defineConfig({
+    build: {
+      sourcemap: true
+    },
+    plugins: [
+      vue(),
+      VitePWA({
+        workbox: {
+          cleanupOutdatedCaches: false
+        },
+        injectRegister: 'auto',
+        devOptions: {
+          enabled: true
+        },
+        manifest: {
+          theme_color: '#114c7c',
+          background_color: '#114c7c', //  114C7C 1B5477
+          display: 'standalone',
+          scope: '/',
+          lang: 'pt',
+          start_url: '/',
+          name: 'Muda',
+          short_name: 'Muda',
+          description: 'Seu melhor jeito para aprender feliz!',
+          icons: [
+            {
+              src: '/logo-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/logo-256x256.png',
+              sizes: '256x256',
+              type: 'image/png'
+            },
+            {
+              src: '/logo-384x384.png',
+              sizes: '384x384',
+              type: 'image/png'
+            },
+            {
+              src: '/logo.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: '/logo-bg.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ],
+          dir: 'auto',
+          categories: [
+            'education',
+            'fitness',
+            'food',
+            'games',
+            'health',
+            'music',
+            'productivity',
+            'social',
+            'utilities'
+          ]
+        }
+      }),
+      sentryVitePlugin({
+        org: p.env.VITE_SENTRY_ORG,
+        project: p.env.VITE_SENTRY_PROJECT,
+        authToken: p.env.VITE_SENTRY_AUTH_TOKEN
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    server: {
+      host: true,
+      port: 8080
     }
-  },
-  server: {
-    host: true,
-    port: 8080
-  }
-})
+  })
+}
